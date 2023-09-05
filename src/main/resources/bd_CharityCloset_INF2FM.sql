db8a15466861142dce981c783682b1f75f3ba4ea
ZsTech <wellingtonzsescola@gmail.com>
2023-09-05 15:51:17 -0300

    sql update

--- src/main/resources/bd_CharityCloset_INF2FM.sql
+++ src/main/resources/bd_CharityCloset_INF2FM.sql
@@ -9,10 +9,7 @@ GO
 
 USE bd_CharityCloset_INF2FM
 
-
-
 --Criação das tabelas:
-
 
 --Usuario:
 CREATE TABLE Usuario
@@ -20,16 +17,16 @@ CREATE TABLE Usuario
 	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	nome			VARCHAR(100)	NOT NULL,
 	sobrenome		VARCHAR(100)	NOT NULL,
-	email			VARCHAR(100)	UNIQUE		NOT NULL,
+	email			VARCHAR(100)	UNIQUE	NOT NULL,
 	senha			VARCHAR(100)	NOT NULL,
 	telefone		VARCHAR(20)		NOT NULL,
 	nivelAcesso		VARCHAR(10)		NULL, --ADMIN ou USER
 	foto			VARBINARY(MAX)	NULL,
 	cpf				VARCHAR(11)		NOT NULL,
-	dataNasc		DATETIME			NOT NULL,
+	dataNasc		DATETIME		NOT NULL,
 	cep				CHAR(7)			NOT NULL,
 	uf				CHAR(2)			NOT NULL,
-	dataCadastro	DATETIME			NOT NULL,
+	dataCadastro	DATETIME		NOT NULL,
 	genero			VARCHAR(10)		NOT NULL,
 	biografia		VARCHAR(200)	NOT NULL,
 	statusUsuario	VARCHAR(20)		NOT NULL,
@@ -68,9 +65,9 @@ CREATE TABLE ConfiguracoesGerais
 	usuario_id				BIGINT			NOT NULL,
 	ong_id					BIGINT			NOT NULL,			
 
-	FOREIGN KEY	(administrador_id) REFERENCES Administrador(id),
-	FOREIGN KEY	(usuario_id) REFERENCES Usuario(id),
-	FOREIGN KEY (ong_id) REFERENCES(id)
+	FOREIGN KEY	(administrador_id)	REFERENCES Administrador(id),
+	FOREIGN KEY	(usuario_id)		REFERENCES Usuario(id),
+	FOREIGN KEY (ong_id)			REFERENCES ONG(id)
 )
 
 --Avalicao:
@@ -90,22 +87,17 @@ CREATE TABLE ONG
 (
 	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	nome			VARCHAR(100)	NOT NULL,
-	email			VARCHAR(100)	UNIQUE		NOT NULL,
+	email			VARCHAR(100)	UNIQUE	NOT NULL,
 	senha			VARCHAR(100)	NOT NULL,
 	telefone		VARCHAR(20)		NOT NULL,
 	nivelAcesso		VARCHAR(10)		NULL, --ADMIN ou USER
 	foto			VARBINARY(MAX)	NULL,
 	cep				CHAR(7)			NOT NULL,
-	dataCadastro	DATETIME			NOT NULL,
+	dataCadastro	DATETIME		NOT NULL,
 	sobreNos		VARCHAR(200)	NOT NULL,
 	uf				CHAR(2)			NOT NULL,
 	cnpj			VARCHAR(14)		NOT NULL,
 	statusONG		VARCHAR(20)		NOT NULL,
-	config_id		BIGINT			NOT NULL,
-	avaliacao_id	BIGINT			NOT NULL,
-
-	FOREIGN KEY	(config_id) REFERENCES ConfiguracoesGerais(id),
-	FOREIGN KEY	(avaliacao_id) REFERENCES Avaliacao(id)
 )
 
 --CategoriaRoupas:
@@ -138,8 +130,10 @@ CREATE TABLE AprovacaoONG
 	dataAprovacao	DATETIME		NOT NULL,
 	statusAprovacao	VARCHAR(20)		NOT NULL,
 	admin_id		BIGINT			NOT NULL,
+	ong_id			BIGINT			NOT NULL,
 
-	FOREIGN KEY (admin_id)	REFERENCES Administrador(id)
+	FOREIGN KEY (admin_id)	REFERENCES Administrador(id),
+	FOREIGN KEY (ong_id)	REFERENCES ONG(id)
 )
 
 --ReprovacaoONG
@@ -149,8 +143,10 @@ CREATE TABLE ReprovacaoONG
 	dataReprovacao	 DATETIME			NOT NULL,
 	statusReprovacao VARCHAR(20)		NOT NULL,
 	admin_id		 BIGINT				NOT NULL,
+	ong_id			 BIGINT				NOT NULL,
 
-	FOREIGN KEY (admin_id)	REFERENCES Administrador(id)
+	FOREIGN KEY (admin_id)	REFERENCES Administrador(id),
+	FOREIGN KEY (ong_id)	REFERENCES ONG(id)
 )
 
 
@@ -159,53 +155,40 @@ CREATE TABLE ReprovacaoONG
 -- resolver erro: Falha ao converter data e/ou hora da cadeia de caracteres. resolvido. rogérião e eu arrumamos
 --Usuario:
 INSERT INTO [dbo].[Usuario](nome, sobrenome, email, senha, telefone, nivelAcesso, foto, cpf, dataNasc, cep, uf, dataCadastro, genero, biografia, statusUsuario)
-VALUES ('zezo', 
-        'lindo',
-		'zezo@zezo.com',
-		'123', 
-		'11952303304', 
-		'ADMIN',
-		null, 
-		'12332112323', 
-		'2021-01-08T00:00:00', 
-		'1231233',
-		'SP',
-		'2000-10-05T00:00:00',
-		'aaaa', 
-		'eu sou o zezo e sou lindo', 
-		'ATIVO')
+VALUES ('zezo', 'lindo', 'zezo@zezo.com', '123', '11952303304', 'ADMIN', null, '12332112323', '2021-01-08T00:00:00', '1231233','SP','2000-10-05T00:00:00','aaaa', 'eu sou o zezo e sou lindo', 'ATIVO')
 
 --ONG:
-INSERT ONG (nome, email, senha, telefone, cep, dataCadastro, sobreNos, uf, cnpj, statusONG) 
-VALUES ('')
+INSERT INTO ONG (nome, email, senha, telefone, nivelAcesso, foto, cep, dataCadastro, sobreNos, uf, cnpj, statusONG) 
+VALUES ('zezo', 'aaa@aaa.com', '111', '11952303304','USER', null, '1231234', '2021-01-08T00:00:00', 'aaaa', 'rj', '11111111111111', 'ativo')
 
 --Administrador:
-INSERT Administrador (cpf, email, nome, sobrenome, senha, uf, dataNasc, cep, dataCadastro, telefone, statusAdmin) 
-VALUES ('')
+INSERT INTO Administrador (cpf, email, nome, sobrenome, senha, uf, dataNasc, cep, dataCadastro, telefone, nivelAcesso, foto, statusAdmin) 
+VALUES ('11111111111', 'AA@AA.COM', 'aaa', 'aaa', '11212', 'pe', '2021-01-08T00:00:00', '1234567', '2021-01-08T00:00:00', '11952303304', 'admin', null, 'ativo')
 
+--perguntar pro cruz oque pode vir aqui
 --ConfiguraçõesGerais:
-INSERT ConfiguracoesGerais (politicasPrivacidade, termosUso, outrasConfiguracoes, statusConfiguracoes)
+INSERT INTO ConfiguracoesGerais (politicasPrivacidade, termosUso, outrasConfiguracoes, statusConfiguracoes)
 VALUES('')
 
 --Avaliacao:
-INSERT Avaliacao (comentario, nota, statusAvaliacao)
-VALUES('')
+INSERT INTO Avaliacao (comentario, nota, statusAvaliacao)
+VALUES('comentário legal', '10', 'ativo')
 
 --Aprovacao:
-INSERT AprovacaoONG (dataAprovacao, statusAprovacao)
-VALUES('')
+INSERT INTO AprovacaoONG (dataAprovacao, statusAprovacao, ong_id)
+VALUES('2021-01-08T00:00:00', 'aprovado', 1)
 
 --Reprovacao:
-INSERT ReprovacaoONG (dataReprovacao, statusReprovacao)
-VALUES('')
+INSERT INTO ReprovacaoONG (dataReprovacao, statusReprovacao, ong_id)
+VALUES('2021-01-08T00:00:00', 'reprovado', 1)
 
 --ContatoSuporte:
-INSERT ContatoSuporte (assunto, mensagem, dataContato, statusContato, usuario_id)
-VALUES('')
+INSERT INTO ContatoSuporte (assunto, mensagem, dataContato, statusContato, usuario_id)
+VALUES('ashasas', 'sdklhjaklsdhj', '2021-01-08T00:00:00', 'iniciado', 1)
 
 --CategoriaRoupas:
-INSERT CategoriaRoupas (genero, tamanho, statusCategoria) 
-VALUES ('')
+INSERT INTO CategoriaRoupas (genero, tamanho, statusCategoria) 
+VALUES ('m', '12', 'ativo')
 
 --Selects:
 
