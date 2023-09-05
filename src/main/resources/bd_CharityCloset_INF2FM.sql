2d250f1d9f8043b8cfbf5414c9dba7c1a01fc3c2
WellingtonZs <wellingtonzsescola@gmail.com>
2023-09-05 11:52:39 -0300

    SQL RESOLVIDOOOOOO

--- src/main/resources/bd_CharityCloset_INF2FM.sql
+++ src/main/resources/bd_CharityCloset_INF2FM.sql
@@ -14,33 +14,10 @@ USE bd_CharityCloset_INF2FM
 --Criação das tabelas:
 
 
---ConfiguracoesGerais:
-CREATE TABLE ConfiguracoesGerais
-(
-	id						BIGINT				IDENTITY,
-	politicasPrivacidade	VARCHAR(200)	NOT NULL,
-	termosUso				VARCHAR(200)	NOT NULL,
-	outrasConfiguracoes		VARCHAR(200)	NOT NULL,
-	statusConfiguracoes		VARCHAR(20)		NOT NULL,
-
-	PRIMARY KEY (id)
-)
-
---Avalicao:
-CREATE TABLE Avaliacao
-(
-	id				BIGINT				IDENTITY,
-	comentario		VARCHAR(100)	NOT NULL,
-	nota			INT				NOT NULL,
-	statusAvaliacao VARCHAR(20)		NOT NULL,
-
-	PRIMARY KEY (id)
-)
-
 --Usuario:
 CREATE TABLE Usuario
 (
-	id				BIGINT			IDENTITY,
+	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	nome			VARCHAR(100)	NOT NULL,
 	sobrenome		VARCHAR(100)	NOT NULL,
 	email			VARCHAR(100)	UNIQUE		NOT NULL,
@@ -49,25 +26,69 @@ CREATE TABLE Usuario
 	nivelAcesso		VARCHAR(10)		NULL, --ADMIN ou USER
 	foto			VARBINARY(MAX)	NULL,
 	cpf				VARCHAR(11)		NOT NULL,
-	dataNasc		DATE			NOT NULL,
+	dataNasc		DATETIME			NOT NULL,
 	cep				CHAR(7)			NOT NULL,
 	uf				CHAR(2)			NOT NULL,
-	dataCadastro	DATE			NOT NULL,
+	dataCadastro	DATETIME			NOT NULL,
 	genero			VARCHAR(10)		NOT NULL,
 	biografia		VARCHAR(200)	NOT NULL,
 	statusUsuario	VARCHAR(20)		NOT NULL,
-	config_id		BIGINT			NOT NULL,
-	avaliacao_id	BIGINT			NOT NULL,
+)
 
-	PRIMARY KEY (id),
-	FOREIGN KEY (config_id) REFERENCES ConfiguracoesGerais(id),
-	FOREIGN KEY	(avaliacao_id) REFERENCES Avaliacao(id)
+--Administrador:
+CREATE TABLE Administrador
+(
+	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	cpf				VARCHAR(11)		NOT NULL,
+	email			VARCHAR(100)	UNIQUE	 NOT NULL,
+	nome			VARCHAR(100)	NOT NULL,
+	sobrenome		VARCHAR(100)	NOT NULL,
+	senha			VARCHAR(100)	NOT NULL,
+	uf				CHAR(2)			NOT NULL,
+	dataNasc		DATETIME		NOT NULL,
+	cep				CHAR(7)			NOT NULL,
+	dataCadastro	DATETIME		NOT NULL,
+	telefone		VARCHAR(20)		NOT NULL,
+	nivelAcesso		VARCHAR(10)		NULL, --ADMIN ou USER
+	foto			VARBINARY(MAX)	NULL,
+	statusAdmin		VARCHAR(20)		NOT NULL,
+
+)
+--editar fks
+
+--ConfiguracoesGerais:
+CREATE TABLE ConfiguracoesGerais
+(
+	id						BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	politicasPrivacidade	VARCHAR(200)	NOT NULL,
+	termosUso				VARCHAR(200)	NOT NULL,
+	outrasConfiguracoes		VARCHAR(200)	NOT NULL,
+	statusConfiguracoes		VARCHAR(20)		NOT NULL,
+	administrador_id		BIGINT			NOT	NULL,
+	usuario_id				BIGINT			NOT NULL,
+	ong_id					BIGINT			NOT NULL,			
+
+	FOREIGN KEY	(administrador_id) REFERENCES Administrador(id),
+	FOREIGN KEY	(usuario_id) REFERENCES Usuario(id),
+	FOREIGN KEY (ong_id) REFERENCES(id)
+)
+
+--Avalicao:
+CREATE TABLE Avaliacao
+(
+	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	comentario		VARCHAR(100)	NOT NULL,
+	nota			INT				NOT NULL,
+	statusAvaliacao VARCHAR(20)		NOT NULL,
+	usuario_id		BIGINT			NOT NULL,
+	
+	FOREIGN KEY	(usuario_id) REFERENCES Usuario(id)
 )
 
 --ONG:
 CREATE TABLE ONG
 (
-	id				BIGINT				IDENTITY,
+	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	nome			VARCHAR(100)	NOT NULL,
 	email			VARCHAR(100)	UNIQUE		NOT NULL,
 	senha			VARCHAR(100)	NOT NULL,
@@ -75,7 +96,7 @@ CREATE TABLE ONG
 	nivelAcesso		VARCHAR(10)		NULL, --ADMIN ou USER
 	foto			VARBINARY(MAX)	NULL,
 	cep				CHAR(7)			NOT NULL,
-	dataCadastro	DATE			NOT NULL,
+	dataCadastro	DATETIME			NOT NULL,
 	sobreNos		VARCHAR(200)	NOT NULL,
 	uf				CHAR(2)			NOT NULL,
 	cnpj			VARCHAR(14)		NOT NULL,
@@ -83,90 +104,76 @@ CREATE TABLE ONG
 	config_id		BIGINT			NOT NULL,
 	avaliacao_id	BIGINT			NOT NULL,
 
-	PRIMARY KEY (id),
-	FOREIGN KEY (config_id) REFERENCES ConfiguracoesGerais(id),
+	FOREIGN KEY	(config_id) REFERENCES ConfiguracoesGerais(id),
 	FOREIGN KEY	(avaliacao_id) REFERENCES Avaliacao(id)
-)
-
---Administrador:
-CREATE TABLE Administrador
-(
-	id				BIGINT			IDENTITY,
-	cpf				VARCHAR(11)		NOT NULL,
-	email			VARCHAR(100)	UNIQUE		NOT NULL,
-	nome			VARCHAR(100)	NOT NULL,
-	sobrenome		VARCHAR(100)	NOT NULL,
-	senha			VARCHAR(100)	NOT NULL,
-	uf				CHAR(2)			NOT NULL,
-	dataNasc		DATE			NOT NULL,
-	cep				CHAR(7)			NOT NULL,
-	dataCadastro	DATE			NOT NULL,
-	telefone		VARCHAR(20)		NOT NULL,
-	nivelAcesso		VARCHAR(10)		NULL, --ADMIN ou USER
-	foto			VARBINARY(MAX)	NULL,
-	statusAdmin		VARCHAR(20)		NOT NULL,
-	config_id		BIGINT			NOT NULL,
-
-	PRIMARY KEY (id),
-	FOREIGN KEY (config_id) REFERENCES ConfiguracoesGerais(id)
 )
 
 --CategoriaRoupas:
 CREATE TABLE CategoriaRoupas
 (
-	id				BIGINT			IDENTITY,
+	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	genero			VARCHAR(10)		NOT NULL,
 	tamanho			VARCHAR(5)		NOT NULL,
 	statusCategoria	VARCHAR(20)		NOT NULL,
 
-	PRIMARY KEY (id)
 )
 
 --ContatoSuporte:
 CREATE TABLE ContatoSuporte
 (
-	id				BIGINT			IDENTITY,
+	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	assunto			VARCHAR(50)		NOT NULL,
 	mensagem		VARCHAR(200)	NOT NULL,
-	dataContato		DATE			NOT NULL,
+	dataContato		DATETIME		NOT NULL,
 	statusContato	VARCHAR(20)		NOT NULL,
 	usuario_id		BIGINT			NOT NULL,
 
-	PRIMARY KEY (id),
 	FOREIGN KEY (usuario_id)	REFERENCES Usuario (id)
 )
 
 --AprovacaoONG
 CREATE TABLE AprovacaoONG
 (
-	id				BIGINT			IDENTITY,
-	dataAprovacao	DATE			NOT NULL,
+	id				BIGINT			NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	dataAprovacao	DATETIME		NOT NULL,
 	statusAprovacao	VARCHAR(20)		NOT NULL,
 	admin_id		BIGINT			NOT NULL,
 
-	PRIMARY KEY (id),
 	FOREIGN KEY (admin_id)	REFERENCES Administrador(id)
 )
 
 --ReprovacaoONG
 CREATE TABLE ReprovacaoONG
 (
-	id				 BIGINT				IDENTITY,
-	dataReprovacao	 DATE				NOT NULL,
+	id				 BIGINT				NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	dataReprovacao	 DATETIME			NOT NULL,
 	statusReprovacao VARCHAR(20)		NOT NULL,
 	admin_id		 BIGINT				NOT NULL,
 
-	PRIMARY KEY (id),
 	FOREIGN KEY (admin_id)	REFERENCES Administrador(id)
 )
 
 
 
 --Inserts das Tabelas:
--- resolver erro: Falha ao converter data e/ou hora da cadeia de caracteres.
+-- resolver erro: Falha ao converter data e/ou hora da cadeia de caracteres. resolvido. rogérião e eu arrumamos
 --Usuario:
-INSERT Usuario(nome, sobrenome, email, senha, telefone, nivelAcesso, foto, cpf, dataNasc, cep, uf, dataCadastro, genero, biografia, statusUsuario) 
-VALUES ('zezo', 'lindo','zezo@zezo.com','123', '11952303304', 'ADMIN',null, '12332112323', '2021-01-08', '1231233', 'SP', '10112011', 'aaaa', 'eu sou o zezo e sou lindo', 'ATIVO')
+INSERT INTO [dbo].[Usuario](nome, sobrenome, email, senha, telefone, nivelAcesso, foto, cpf, dataNasc, cep, uf, dataCadastro, genero, biografia, statusUsuario)
+VALUES ('zezo', 
+        'lindo',
+		'zezo@zezo.com',
+		'123', 
+		'11952303304', 
+		'ADMIN',
+		null, 
+		'12332112323', 
+		'2021-01-08T00:00:00', 
+		'1231233',
+		'SP',
+		'2000-10-05T00:00:00',
+		'aaaa', 
+		'eu sou o zezo e sou lindo', 
+		'ATIVO')
 
 --ONG:
 INSERT ONG (nome, email, senha, telefone, cep, dataCadastro, sobreNos, uf, cnpj, statusONG) 
