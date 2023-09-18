be8a15b4a238dff019d28392e13bfc220191dd6b
LecoTV <lecotv2006123@gmail.com>
2023-09-18 07:25:51 -0300

    backend update

--- src/main/java/com/tcc/charitycloset/inf2fm/charityclosetinf2fm/repository/UsuarioRepository.java
+++ src/main/java/com/tcc/charitycloset/inf2fm/charityclosetinf2fm/repository/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package com.tcc.charitycloset.inf2fm.charityclosetinf2fm.repository;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tcc.charitycloset.inf2fm.charityclosetinf2fm.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 }
