b695e1375756db6fed24db6d16f8fa6c39f6d537
Cauã Santana <114744734+emostalooknegativeqi@users.noreply.github.com>
2024-03-15 17:07:28 -0300

    enviando as alterações que o alves pediu pra enviar (TO ESCUTANDO PITTY EU AMO ESSA MULHER)

--- src/main/resources/static/script/userAdm.js
+++ src/main/resources/static/script/userAdm.js
@@ -1,8 +1,86 @@
-const btnUser = document.getElementById("input");
+let title = document.querySelectorAll('#title-categoria .item');
+let item = document.querySelectorAll('#info .item');
+let item2 = document.querySelectorAll('#info2 .item');
+let item3 = document.querySelectorAll('#info3 .item');
+let item4 = document.querySelectorAll('#info4 .item');
+let item5 = document.querySelectorAll('#info5 .item');
+let item6 = document.querySelectorAll('#info6 .item');
 
-function selecionar () {
-    console.log("funcionando")
+
+function mudarOngs() {
+console.log(item);
+title[0].textContent = " ONG";
+title[1].textContent = "Representante";
+title[2].textContent = "E-mail";
+title[3].textContent = "Senha";
+
+item[0].textContent = " LIFEOFPABLO";
+item[1].textContent = "	CAUÃ";
+item[2].textContent = "	C@HORSE.COM";
+item[3].textContent = "FREDDIEMERCURY";
+
+item2[0].textContent = "GRADUATION";
+item2[1].textContent = " GUSTAVO";
+item2[2].textContent = " G@HORSE.COM";
+item2[3].textContent = " AMYWINEHOUSE";
+
+item3[0].textContent = "YEEZUS";
+item3[1].textContent = "LEANDRO";
+item3[2].textContent = "L@HORSE.COM";
+item3[3].textContent = "FRANKSINATRA";
+
+item4[0].textContent = " YE";
+item4[1].textContent = "NATHALIA";
+item4[2].textContent = "N@HORSE.COM";
+item4[3].textContent = "OZZYOSBOURNE";
+
+item5[0].textContent = " DONDA";
+item5[1].textContent = "RUAN";
+item5[2].textContent = "I@HORSE.COM";
+item5[3].textContent = "MICHAELJACKSON";
+
+item6[0].textContent = " WATCHTHRONE";
+item6[1].textContent = "WELLINGTON";
+item6[2].textContent = "W@HORSE.COM";
+item6[3].textContent = " JOHNLENNON";
 }
 
+function mudarUser() {
+    title[0].textContent = ""
+    title[1].textContent = "Nome";
+    title[2].textContent = "E-mail";
+    title[3].textContent = "Senha";
 
-btnUser.addEventListener('click', selecionar)
+    item[0].textContent = " ";
+    item[1].textContent = "	JAY JO";
+    item[2].textContent = "	WINDBREAKER@GMAIL.COM";
+    item[3].textContent = "ILOVEBIKES";
+
+    item2[0].textContent = " ";
+    item2[1].textContent = "DOM KANG";
+    item2[2].textContent = "SPEEDFORCE@GMAIL.COM";
+    item2[3].textContent = "MAFIADOSCACHIMBINHAS";
+
+    item3[0].textContent = " ";
+    item3[1].textContent = "SHELLY SCOTT";
+    item3[2].textContent = "HUMMINGBIRD@GMAIL.COM";
+    item3[3].textContent = "IloveJayJO";
+
+    item4[0].textContent = " ";
+    item4[1].textContent = "OWEN KNIGHT";
+    item4[2].textContent = "LIGHTCAVALRY@GMAIL.COM";
+    item4[3].textContent = "ILOVESHELLY";
+
+    item5[0].textContent = " ";
+    item5[1].textContent = "JOKER";
+    item5[2].textContent = "SABBATH@GMAIL.COM";
+    item5[3].textContent = "ILOVECATS";
+
+    item6[0].textContent = " ";
+    item6[1].textContent = "Kaneshiro";
+    item6[2].textContent = "KAZUMA@GMAIL.COM";
+    item6[3].textContent = "ILOVECROE";
+
+
+}
+ 
